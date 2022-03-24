06802227afb28e6bb47fe6d11285f6417801d6ce
Muhammad Fakhri Robbani <muhammad.fakhri1027@students.unila.ac.id>
2022-03-24 18:43:21 +0700

    menambahkan tugas function

--- frontend/javascript/6-function-cp/script-1.js
+++ frontend/javascript/6-function-cp/script-1.js
@@ -7,6 +7,18 @@
 
 function reverseString(str) {
     // TODO: answer here
+    // cara 1
+    var kata = '';
+    for (let i = str.length - 1; i >= 0; i--) {  
+        kata += str[i];
+    }
+    return kata;
+
+    // cara 2
+    // var pisahTeks = str.split('');
+    // var balikTeks = pisahTeks.reverse();
+    // var gabungTeks = balikTeks.join('');
+    // return gabungTeks;
 }
 
 const string = prompt('Masukan teks: ');
--- frontend/javascript/6-function-cp/script-2.js
+++ frontend/javascript/6-function-cp/script-2.js
@@ -9,8 +9,19 @@
 // teks2 dibalik sama menjadi madam, maka balikan akan true
 // teks3 dibalik sama menjadi kasur ini rusak, maka balikan akan true
 
-function checkPalindrome(string) {
+function checkPalindrome(teks) {
     // TODO: answer here
+    // cara1
+    var kata = '';
+    for (let i = teks.length - 1; i >= 0; i--) {  
+        kata += teks[i];
+    }
+    return kata === teks
+    // cara2
+    // var pisahTeks = teks.split('');
+    // var balikTeks = pisahTeks.reverse();
+    // var gabungTeks = balikTeks.join('');
+    // return gabungTeks === teks;
 }
 
 // masukan teks
