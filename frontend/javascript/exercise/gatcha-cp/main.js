b8a61afbbc4d35fd824ffce53a29cdb4621ed96a
Muhammad Fakhri Robbani <muhammad.fakhri1027@students.unila.ac.id>
2022-03-29 19:27:07 +0700

    submit exercise gatcha-cp

--- frontend/javascript/exercise/gatcha-cp/main.js
+++ frontend/javascript/exercise/gatcha-cp/main.js
@@ -14,10 +14,22 @@
 
 function gatcha(button) {
   // TODO: answer here
+  switch (button) {
+    case 1:
+      return "coba lagi ya";
+    case 2:
+      return "selamat kamu mendapatkan kupon sebanyak 5"
+    case 3:
+      return "selamat kamu mendapatkan kupon sebanyak 15"
+    case 4:
+      return "selamat kamu mendapatkan kupon sebanyak 50"
+    case 5:
+      return "selamat kamu mendapatkan kupon sebanyak 100"
+  }
 }
-
 // Create variable button here
 // TODO: answer here
+var button = Math.floor(Math.random() * 5) + 1;
 
 console.log(gatcha(button))
 
