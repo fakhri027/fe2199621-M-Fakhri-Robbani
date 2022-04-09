65194f3f2159e6607dc4da37d4fad11d22547247
Muhammad Fakhri Robbani <muhammad.fakhri1027@students.unila.ac.id>
2022-04-09 19:43:39 +0700

    update

--- frontend/dom/calculator-cp/index.js
+++ frontend/dom/calculator-cp/index.js
@@ -1,26 +1,27 @@
 //handle when the number is pressed. It renders the number into #input element
 function number(number) {
     // TODO: answer here
-let input = document.querySelector("#input");
-input.value += number;
+    let input = document.querySelector("#input");
+    input.value += number;
 }
 
 //handle when the Del button, AC button and other operators (+, /, -, x) are pressed. It renders the operator into #input element
 function operator(operator) {
     // TODO: answer here
-let input = document.querySelector("#input");
-input.value += operator;
-if (operator === "AC") {
-    input.value = "";
-}
-if (operator === "Del") {
-    input.value = input.value.slice(0, -4);
-}
+    let input = document.querySelector("#input");
+
+    if (operator === "AC") {
+        input.value = "";
+    } else if (operator === "Del") {
+        input.value = input.value.slice(0, -1);
+    } else {
+        input.value += operator
+    }
 }
 
 //handle when = button is pressed. It renders the result into #input element
 function calculate() {
     // TODO: answer here
-let input = document.querySelector("#input");
-input.value = eval(input.value);
+    let input = document.querySelector("#input");
+    input.value = eval(input.value);
 }
--- frontend/dom/tictactoe-assignment/index.js
+++ frontend/dom/tictactoe-assignment/index.js
@@ -9,7 +9,10 @@ let winningCount = {
 //render scoreboard on x-wins and o-wins <label/>
 function renderScore() {
     // TODO: answer here
+    document.getElementById("x-wins").innerHTML = winningCount.X;
+  document.getElementById("o-wins").innerHTML = winningCount.O;
 }
+
 
 //check who the winner is, add the score to the scoreboard, and render the scoreboard
 function checkWinner() {
@@ -60,6 +63,7 @@ function checkNoWinner() {
         for (let j = 0; j < SIZE; j++) {
             if (getValue(i, j) == "") {
                 // TODO: answer here
+                return false;
             }
         }
     }
@@ -73,8 +77,10 @@ function click(event) {
 
     if (turn == "X") {
         // TODO: answer here
+        turn = "O";
     } else {
         // TODO: answer here
+        turn = "X";
     }
 
     checkWinner()
@@ -104,6 +110,7 @@ function generate() {
             button.style.width = BUTTON_SIZE;
             button.style.height = BUTTON_SIZE;
             // TODO: answer here
+            button.addEventListener("click", click);
             button.id = i + "-" + j;
 
             td.appendChild(button);
