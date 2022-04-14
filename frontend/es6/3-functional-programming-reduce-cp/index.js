11252941888864f41e75ba5c3913b9a621eb3349
Muhammad Fakhri Robbani <muhammad.fakhri1027@students.unila.ac.id>
2022-04-14 21:04:00 +0700

    update reduce-cp es6

--- frontend/es6/3-functional-programming-reduce-cp/index.js
+++ frontend/es6/3-functional-programming-reduce-cp/index.js
@@ -7,6 +7,10 @@
 
 const sumTotalArray = (numbers) => {
   // TODO: answer here
+  return numbers.reduce((previous, current) => {
+    return previous + current;
+  }, 0);
 };
-
+const numbers = [1, 1, 1, 5];
+console.log(sumTotalArray(numbers)); // 8
 module.exports = sumTotalArray
