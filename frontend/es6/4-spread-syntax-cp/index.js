c462862159ed78833199d9d908e9a05d7eb47345
Muhammad Fakhri Robbani <muhammad.fakhri1027@students.unila.ac.id>
2022-04-14 21:42:58 +0700

    update spread-syntax-cp es6

--- frontend/es6/4-spread-syntax-cp/index.js
+++ frontend/es6/4-spread-syntax-cp/index.js
@@ -9,6 +9,19 @@
  */
 const max = (...args) => {
   // TODO: answer here
+  let max = args[0];
+  for (let i = 0; i < args.length; i++) {
+    if (args[i] > max) {
+      max = args[i];
+    } else{
+      max = max;
+    } 
+  } return max;
 };
-
+const result = max(1, 2, 3, 4, 12, 3, 4);
+const result2 = max(1, 2, 5);
+const result3 = max(1);
+console.log(result);
+console.log(result2);
+console.log(result3);
 module.exports = max
