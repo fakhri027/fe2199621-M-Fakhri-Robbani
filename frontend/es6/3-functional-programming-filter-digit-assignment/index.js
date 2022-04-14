96373ed11f2d38a2fe3273f3172e02cb00ebfa51
Muhammad Fakhri Robbani <muhammad.fakhri1027@students.unila.ac.id>
2022-04-14 23:15:50 +0700

    update filter-digit-assignment es6

--- frontend/es6/3-functional-programming-filter-digit-assignment/index.js
+++ frontend/es6/3-functional-programming-filter-digit-assignment/index.js
@@ -11,6 +11,13 @@
 
 const filterDigit = (array, digit) => {
   // TODO: answer here
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (String(array[i]).length === digit) {
+      result.push(array[i]);
+    } 
+  } return result;
 };
-
+// const input = [88, 44, 3, 8481, 444];
+// console.log(filterDigit(input, 2));
 module.exports = filterDigit
