a43acbfd07698a7866dadb25a76e4da567fd9ad6
Muhammad Fakhri Robbani <muhammad.fakhri1027@students.unila.ac.id>
2022-04-26 18:31:20 +0700

    update

--- frontend/data-structure/assignment/parentheses-validation/index.js
+++ frontend/data-structure/assignment/parentheses-validation/index.js
@@ -2,7 +2,36 @@ const Stack = require('./stack')
 
 function IsValidParentheses(s) {
     // TODO: answer here
+    if (s === "") {
+        return false
+    }
+    let stack = new Stack()
+    let map = {
+        '{': '}',
+        '[': ']',
+        '(': ')'
+    }   
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] in map) {
+            stack.push(s[i])
+        } else {
+            if (stack.peek() === map[s[i]]) {
+                stack.pop()
+            } else {
+                return false
+            }
+        }
+    }
+
 }
+// console.log(IsValidParentheses(""));    //false
+// console.log(IsValidParentheses("()"));  //true
+// console.log(IsValidParentheses("()[]{}"));  //true
+// console.log(IsValidParentheses("{[()]"));   //false
+// console.log(IsValidParentheses("[()]}"));   //false
+// console.log(IsValidParentheses("[{)]"));    //false
+// console.log(IsValidParentheses("({{"));    //false
+// console.log(IsValidParentheses("]])"));   //false
 
 module.exports = {
     IsValidParentheses
--- frontend/data-structure/assignment/text-editor/index.js
+++ frontend/data-structure/assignment/text-editor/index.js
@@ -1,21 +1,44 @@
 module.exports = class TextEditor {
     constructor() {
         // TODO: answer here
+        this.data = [];
+        this.top = -1;
+        this.undoStack = [];
+        this.redoStack = [];
     }
 
     write(c) {
         // TODO: answer here
+        const ch = c.charAt(0);
+        this.data.push(ch);
+        this.top += 1;
+        this.undoStack.push(ch);
     }
 
     read() {
         // TODO: answer here
+        return this.data.join('');
     }
 
     undo() {
         // TODO: answer here
+        if (this.undoStack.length === 0) {
+            return;
+        }   
+        const ch = this.undoStack.pop();
+        this.redoStack.push(ch);
+        this.top -= 1;
+        this.data.pop();
     }
     
     redo() {
         // TODO: answer here
+        if (this.redoStack.length === 0) {
+            return;
+        }   
+        const ch = this.redoStack.pop();
+        this.undoStack.push(ch);
+        this.top += 1;
+        this.data.push(ch);
     }
 }
